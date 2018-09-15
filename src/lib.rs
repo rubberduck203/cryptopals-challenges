4944b0ba9c9a5f719a14e1bc55fa8781359ea7b9
Christopher J. McClellan <Chris.McClellan203@gmail.com>
2018-09-15 19:27:06 -0400

    Convert hex string to base64 string

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 mod basics {
-    fn hex_to_base64(_hex_as_string: &str) -> &'static str {
-        ""
+    extern crate base64;
+    use self::base64::encode;
+
+    pub fn hex_to_base64(hex: &str) -> String {
+        encode(&hex_to_bytes(hex))
     }
 
-    fn hex_to_bytes(hex: &str) -> Vec<u8> {
+    pub fn hex_to_bytes(hex: &str) -> Vec<u8> {
         hex.chars()
             .collect::<Vec<_>>() //convert to slice-able
             .chunks(2)           //each char is one nibble
@@ -17,10 +20,9 @@ mod basics {
         use super::*;
 
         #[test]
-        #[ignore]
         fn can_convert_hex_to_base64() {
             let hex_as_string =
-                 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f7d";
+                "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
             let base64_as_string =
                 "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
 
