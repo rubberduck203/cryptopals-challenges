3115177ca9c910a1a40362ff56bd92da8fb39065
Christopher J. McClellan <Chris.McClellan203@gmail.com>
2018-09-15 20:19:47 -0400

    xor equal length byte arrays

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 http://cryptopals.com/
 
+
+## Challenges Completed
+
+- [x] [Convert hex to base64](http://cryptopals.com/sets/1/challenges/1)
+- [x] [Fixed XOR](http://cryptopals.com/sets/1/challenges/2)
+- [ ] [Single-byte XOR cipher](http://cryptopals.com/sets/1/challenges/3)
+
+## TODO
+
+- [ ] Extract `hex` module into it's own file
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,35 @@
-mod basics {
+pub mod byte_array {
+    pub fn xor(a: &[u8], b: &[u8]) -> Vec<u8> {
+        a.iter().zip(b.iter()).map(|(x, y)| x ^ y).collect()
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        use hex;
+
+        #[test]
+        fn xor_two_equal_length_byte_arrays() {
+            let hex_a = hex::to_bytes("1c0111001f010100061a024b53535009181c");
+            let hex_b = hex::to_bytes("686974207468652062756c6c277320657965");
+
+            assert_eq!(
+                hex::to_bytes("746865206b696420646f6e277420706c6179"),
+                xor(&hex_a, &hex_b),
+            );
+        }
+    }
+}
+
+pub mod hex {
     extern crate base64;
     use self::base64::encode;
 
-    pub fn hex_to_base64(hex: &str) -> String {
-        encode(&hex_to_bytes(hex))
+    pub fn to_base64(hex: &str) -> String {
+        encode(&to_bytes(hex))
     }
 
-    pub fn hex_to_bytes(hex: &str) -> Vec<u8> {
+    pub fn to_bytes(hex: &str) -> Vec<u8> {
         hex.chars()
             .collect::<Vec<_>>() //convert to slice-able
             .chunks(2)           //each char is one nibble
@@ -26,26 +49,26 @@ mod basics {
             let base64_as_string =
                 "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
 
-            assert_eq!(hex_to_base64(hex_as_string), base64_as_string);
+            assert_eq!(to_base64(hex_as_string), base64_as_string);
         }
 
         #[test]
         fn ff_byte_hex_string_to_byte_vector() {
             let hex = "FF";
 
-            assert_eq!(vec![0xFF], hex_to_bytes(hex));
+            assert_eq!(vec![0xFF], to_bytes(hex));
         }
 
         #[test]
         fn single_byte_hex_string_to_byte_vector() {
             let hex = "2A";
-            assert_eq!(vec![0x2A], hex_to_bytes(hex));
+            assert_eq!(vec![0x2A], to_bytes(hex));
         }
 
         #[test]
         fn multibyte_hex_string_to_byte_vector() {
             let hex = "2ABE";
-            assert_eq!(vec![0x2A, 0xBE], hex_to_bytes(hex));
+            assert_eq!(vec![0x2A, 0xBE], to_bytes(hex));
         }
     }
 }
